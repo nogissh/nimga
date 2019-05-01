a40e1a86f66dea6d222a7c7f15772dfa0e063e77
Toshiki Ohnogi <toshikiohnogi@to-base.local>
2019-05-01 19:58:33 +0900

    Update: Mutation

---
+++ src/mutation.nim
@@ -0,0 +1,30 @@
+from random import rand
+from objects/basic import Gene
+
+
+proc willMutate*(mutationProb=0.1): bool =
+  ##
+  ## Decide do mutation
+  ##
+  if rand(1.0) <= mutationProb:
+    result = true
+  else:
+    result = false
+
+
+proc runMutation*(genes: seq[Gene]): seq[Gene] =
+  ##
+  ## Mutate chrome twice of genes
+  ##
+  result = genes # Maybe passed by reference
+
+  var target, a, b, tmp: int
+
+  target = rand(genes.len)
+  a      = rand(genes[0].chrom.len)
+  b      = rand(genes[0].chrom.len)
+
+  # Replace chrome
+  tmp                     = result[target].chrom[a]
+  result[target].chrom[a] = result[target].chrom[b]
+  result[target].chrom[b] = tmp
